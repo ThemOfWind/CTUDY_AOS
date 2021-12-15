6e11ee3900786e17b72acf3f326bee514ae549aa
yehyun <into1023@naver.com>
2021-12-15 18:00:06 +0900

    git test

--- app/src/main/java/com/toy/project/ctudy/viewmodel/BaseViewModel.kt
+++ app/src/main/java/com/toy/project/ctudy/viewmodel/BaseViewModel.kt
@@ -8,7 +8,7 @@ import io.reactivex.disposables.Disposable
  * Rxjava2 CompositeDisposeable class 사용
  * Observer를 android lifeCycle에 맞춰 해제할 수 있다
  *
- * 참고 : https://taehyungk.github.io/posts/android-RxJava2-Disposable/
+ * 참고 : https://taehyungk.github.io/posts/android-RxJava2-Disposable//
  */
 open class BaseViewModel : ViewModel() {
     private var compositeDisposable = CompositeDisposable()
