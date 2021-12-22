bc7b5b11dd7140d3a3626d935ce53a7f32a0da22
yehyun <into1023@naver.com>
2021-12-22 17:51:29 +0900

    Login layout 구현 , ViewModel 구현 진행

--- app/src/main/java/com/toy/project/ctudy/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/toy/project/ctudy/viewmodel/LoginViewModel.kt
@@ -1,7 +1,19 @@
 package com.toy.project.ctudy.viewmodel
 
+import com.toy.project.ctudy.repository.network.ApiService
+import com.toy.project.ctudy.repository.pref.UserPref
+
 /**
  * Login ViewModel
  */
-class LoginViewModel : BaseViewModel() {
+class LoginViewModel(
+    private val apiService: ApiService,
+    private val userPref: UserPref,
+) : BaseViewModel() {
+
+    init {
+        if (userPref.getLoginToken()!!.isNotEmpty()) {
+            
+        }
+    }
 }
