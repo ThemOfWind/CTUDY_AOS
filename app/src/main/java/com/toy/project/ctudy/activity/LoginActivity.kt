4ddf7e68dafe470575cda8f73315c58e13a1b11d
yehyun <into1023@naver.com>
2021-12-24 15:04:52 +0900

    viewModel Variable 추가

--- app/src/main/java/com/toy/project/ctudy/activity/LoginActivity.kt
+++ app/src/main/java/com/toy/project/ctudy/activity/LoginActivity.kt
@@ -1,7 +1,6 @@
 package com.toy.project.ctudy.activity
 
 import android.os.Bundle
-import android.view.View
 import com.toy.project.ctudy.R
 import com.toy.project.ctudy.databinding.ActivityLoginBindingImpl
 import com.toy.project.ctudy.viewmodel.LoginViewModel
@@ -10,18 +9,11 @@ import org.koin.androidx.viewmodel.ext.android.viewModel
 /**
  * Login Page
  */
-class LoginActivity : BaseActivity<ActivityLoginBindingImpl, LoginViewModel>(),
-    View.OnClickListener {
+class LoginActivity : BaseActivity<ActivityLoginBindingImpl, LoginViewModel>() {
     override val layoutResID: Int = R.layout.activity_login
     override val viewModel: LoginViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-    }
-
-    override fun onClick(view: View?) {
-        when (view?.id) {
-            viewBinding.loginBtn -> viewModel.doLogin()
-        }
     }
 }
